e45e729dad5d5128a2813fdf53e3d053c06d25c8
Naman Muley <naman.g.muley@gmail.com>
2025-07-25 00:09:56 +0530

    Add src/notification/NotificationActions.js

---
+++ src/notification/NotificationActions.js
@@ -0,0 +1,4 @@
+// Notification action type constants
+export const ADD_NOTIFICATION = "ADD_NOTIFICATION";
+export const MARK_AS_READ = "MARK_AS_READ";
+export const DISMISS_NOTIFICATION = "DISMISS_NOTIFICATION";
